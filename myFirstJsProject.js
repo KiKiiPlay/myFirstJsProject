203541d2912bbd30dcd321bd5306152af918e7fe
Kiril Vandzsurak <kiril.wandzurak@gmail.com>
2022-10-04 14:00:05 +0200

    Personal info in JS file

---
+++ myFirstJsProject.js
@@ -0,0 +1,13 @@
+let myFirstName = "Kiril";
+let myLastName = "Vandzsurak";
+let space = " ";
+
+let myFullName = myFirstName + space + myLastName;
+
+console.log("hello, my name is", myFullName);
+
+let myAge = 18;
+let currentYear = 2022;
+let myBirthYear;
+
+console.log("I was born in", (myBirthYear = currentYear - myAge));
